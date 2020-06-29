1bb331aaff30a640b840e5107dc1f0f76b2a6755
dobrovolnikMarek <47030585+dobrovolnikMarek@users.noreply.github.com>
2020-06-29 15:36:47 +0300

    vue-politology-test

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png"><br>
+    <button v-if="!isStarted" @click="isTest = true, isStarted = true, firstQuestion()" class="btn">Start</button>
+    <Test 
+        v-else-if="isTest" 
+        @res="mark = $event" 
+        :isTest="true"
+        :q = Question
+        :qCounter = counter
+        @testFalse="isTest = $event"/>
+    <Result v-else :mark="mark"/>  
+  </div>
+</template>
+
+<script>
+
+import Test from './components/Test.vue'
+import Result from './components/Result.vue'
+export default {
+  name: 'App',
+  // props:['mark'],
+  data(){
+    return{
+      isStarted: false,
+      isTest: false,
+      mark: 0,
+      counter: 0,
+      Question: 0
+    }
+  },
+  methods:{
+    result: function(){
+      console.log(this.mark);
+  },
+  firstQuestion: function() {
+    this.Question = Math.floor(Math.random() * (185 - 0) + 0);
+    this.counter++;
+    console.log(this.Question);
+  }
+    
+  },
+  components: {
+    Test,
+    Result
+  },
+  computed:{
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img{
+  width: 150px;
+  height: 150px;
+}
+
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>
+          Результат
+      </h1>
+      <h2>
+          {{Math.round(mark)}}
+      </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    props:["mark"]
+}
+</script>
+
+<style>
+
+</style>
